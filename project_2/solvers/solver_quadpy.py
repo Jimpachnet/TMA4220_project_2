e288e9e3566941dd495696689cf1ddedd507f092
Leon Sievers <leon.sievers@tum.de>
2018-11-14 18:25:12 +0100

    WIP

--- project_2/solvers/solver_quadpy.py
+++ project_2/solvers/solver_quadpy.py
@@ -4,9 +4,10 @@ Implements the solver using the quadpy package for integration
 
 import numpy as np
 import quadpy as qp
-from scipy.sparse import lil_matrix
+from scipy.sparse import lil_matrix,csr_matrix
 import matplotlib.pyplot as plt
 import scipy.io
+import scipy.sparse.linalg
 import scipy.io as sio
 from project_2.infrastructure.p1_reference_element import P1ReferenceElement
 from project_2.infrastructure.affine_transformation import AffineTransformation
@@ -39,7 +40,7 @@ def solve_quadpy(mesh,showstats=False):
     bc_count = 0
     for i in range(nr):
         if True:
-            if mesh.supports[i, 2] == 0:
+            if mesh.supports[i, 2] < -0.25:
                 bc_count+=1
                 K[i*3, :] = np.zeros((1, nr*3))
                 K[i*3, i*3] = 1
@@ -94,8 +95,9 @@ def solve_quadpy(mesh,showstats=False):
         print("[Info] Rank:"+str(np.linalg.matrix_rank(K))+"/"+str(K.shape[0]))
 
     print("[Info] Solving system")
-    u = np.squeeze(np.linalg.solve(K,b))
-
+    #u = np.squeeze(np.linalg.solve(K,b))
+    #u = np.linalg.solve(K,b)
+    u = scipy.sparse.linalg.spsolve(K,b)
     ux = u[0::3]
     uy = u[1::3]
     uz = u[2::3]
@@ -115,6 +117,17 @@ def generate_stress(ux,uy,uz,mesh):
     strain = np.zeros((varnr,6))
     stress = np.zeros((varnr, 6))
 
+    Youngs_E_Modulus = 250 * 10 ** 9
+    v = 0.3
+    lame_lambda = Youngs_E_Modulus * v / ((1 + v) * (1 - 2 * v))
+    mue = Youngs_E_Modulus / (2 * (1 + v))
+    D = np.array([[lame_lambda + 2 * mue, lame_lambda, lame_lambda, 0, 0, 0],
+                  [lame_lambda, lame_lambda + 2 * mue, lame_lambda, 0, 0, 0],
+                  [lame_lambda, lame_lambda, lame_lambda + 2 * mue, 0, 0, 0], [0, 0, 0, mue, 0, 0],
+                  [0, 0, 0, 0, mue, 0], [0, 0, 0, 0, 0, mue]])
+
+    co = (.1, .1, .1)
+
     for n in range(varnr):
         v0_coord = (mesh.supports[mesh.tetraeders[n,0],0],mesh.supports[mesh.tetraeders[n,0],1],mesh.supports[mesh.tetraeders[n,0],2])
         v1_coord = (mesh.supports[mesh.tetraeders[n,1],0],mesh.supports[mesh.tetraeders[n,1],1],mesh.supports[mesh.tetraeders[n,1],2])
@@ -124,13 +137,7 @@ def generate_stress(ux,uy,uz,mesh):
         atraf.set_target_cell(v0_coord, v1_coord, v2_coord,v3_coord)
         jinvt = atraf.get_inverse_jacobian()
 
-        Youngs_E_Modulus = 250 * 10**9
-        v = 0.3
-        lame_lambda = Youngs_E_Modulus * v / ((1 + v) * (1 - 2 * v))
-        mue = Youngs_E_Modulus/(2*(1+v))
-        D = np.array([[lame_lambda+2*mue, lame_lambda, lame_lambda, 0, 0, 0], [lame_lambda, lame_lambda+2*mue, lame_lambda, 0, 0, 0], [lame_lambda, lame_lambda, lame_lambda+2*mue, 0, 0, 0],[0,0,0,mue,0,0],[0,0,0,0,mue,0],[0,0,0,0,0,mue]])
 
-        co = (.1,.1,.1)
 
         #Todo: Vectorize
         #Structure:
@@ -197,7 +204,9 @@ def generate_linear_form(mesh):
 def generate_stiffness_matrix(mesh):
     varnr = mesh.supports.shape[0]
     p1_ref = P1ReferenceElement()
-    K = np.zeros((varnr*3, varnr*3))
+    #K = np.zeros((varnr*3, varnr*3))
+    #K = lil_matrix(varnr*3,varnr*3)
+    K = csr_matrix(varnr*3,varnr*3)
     atraf = AffineTransformation()
     for n in range(mesh.tetraeders.shape[0]):
         v0_coord = (mesh.supports[mesh.tetraeders[n,0],0],mesh.supports[mesh.tetraeders[n,0],1],mesh.supports[mesh.tetraeders[n,0],2])
@@ -328,8 +337,28 @@ def generate_stiffness_matrix(mesh):
 def generate_stiffness_matrix_paper(mesh):
     varnr = mesh.supports.shape[0]
     p1_ref = P1ReferenceElement()
-    K = np.zeros((varnr*3, varnr*3))
+    #K = np.zeros((varnr*3, varnr*3))
+    K = lil_matrix((varnr * 3, varnr * 3))
+    #K = csr_matrix((varnr*3,varnr*3))
     atraf = AffineTransformation()
+
+    Youngs_E_Modulus = 250 * 10 ** 9
+    v = 0.3
+
+    lame_lambda = Youngs_E_Modulus * v / ((1 + v) * (1 - 2 * v))
+    mue = Youngs_E_Modulus / (2 * (1 + v))
+
+    fracter = 1
+    D_0 = np.array([[1, v, v, 0, 0, 0], [v, 1, v, 0, 0, 0], [v, v, 1, 0, 0, 0], [0, 0, 0, (1 - v) / fracter, 0, 0],
+                    [0, 0, 0, 0, (1 - fracter) / 2, 0], [0, 0, 0, 0, 0, (1 - v) / fracter]])
+    D_00 = np.array([[lame_lambda + 2 * mue, lame_lambda, lame_lambda, 0, 0, 0],
+                     [lame_lambda, lame_lambda + 2 * mue, lame_lambda, 0, 0, 0],
+                     [lame_lambda, lame_lambda, lame_lambda + 2 * mue, 0, 0, 0], [0, 0, 0, mue, 0, 0],
+                     [0, 0, 0, 0, mue, 0], [0, 0, 0, 0, 0, mue]])
+    # D = Youngs_E_Modulus / (1 - v ** 2) * D_0
+    D = D_00
+
+
 
     for n in range(mesh.tetraeders.shape[0]):
         v0_coord = (mesh.supports[mesh.tetraeders[n,0],0],mesh.supports[mesh.tetraeders[n,0],1],mesh.supports[mesh.tetraeders[n,0],2])
@@ -337,35 +366,14 @@ def generate_stiffness_matrix_paper(mesh):
         v2_coord = (mesh.supports[mesh.tetraeders[n,2],0],mesh.supports[mesh.tetraeders[n,2],1],mesh.supports[mesh.tetraeders[n,2],2])
         v3_coord = (mesh.supports[mesh.tetraeders[n,3],0],mesh.supports[mesh.tetraeders[n,3],1],mesh.supports[mesh.tetraeders[n,3],2])
 
-        atraf.set_target_cell(v0_coord, v1_coord, v2_coord,v3_coord)
-        jinvt = atraf.get_inverse_jacobian().T
 
 
-        Youngs_E_Modulus = 250 * 10**9
-        v = 0.3
-
-        lame_lambda = Youngs_E_Modulus * v / ((1 + v) * (1 - 2 * v))
-        mue = Youngs_E_Modulus/(2*(1+v))
-
-        fracter = 1
-        D_0 = np.array([[1, v, v, 0, 0, 0], [v, 1, v, 0, 0, 0], [v, v, 1, 0, 0, 0],[0,0,0,(1 - v) / fracter,0,0],[0,0,0,0,(1 - fracter) / 2,0],[0,0,0,0,0,(1 - v) / fracter]])
-        D_00 = np.array([[lame_lambda+2*mue, lame_lambda, lame_lambda, 0, 0, 0], [lame_lambda, lame_lambda+2*mue, lame_lambda, 0, 0, 0], [lame_lambda, lame_lambda, lame_lambda+2*mue, 0, 0, 0],[0,0,0,mue,0,0],[0,0,0,0,mue,0],[0,0,0,0,0,mue]])
-        #D = Youngs_E_Modulus / (1 - v ** 2) * D_0
-        D = D_00
-
-
-
-        #D_0_2 = np.array([[1, v/(1-v), 0], [v/(1-v), 1, 0], [0, 0, (1 - 2*v) / 2*(1-v)]])
-
-        #D = Youngs_E_Modulus*(1-v)/((1+v)*(1-2*v))
-
-        local_K = np.zeros((12,12))
 
         PhiGrad = np.ones((4,4))
         vertices_ = np.squeeze(np.array([v0_coord, v1_coord, v2_coord,v3_coord]))
         PhiGrad[1:4,0:] = vertices_.T
         detgiv = PhiGrad
-        PhiGrad = np.linalg.inv(PhiGrad).dot(np.squeeze(np.array([[0,0,0], [1,0,0], [0,1,0], [0,0,1]])))
+        PhiGrad = np.linalg.solve(PhiGrad,np.squeeze(np.array([[0,0,0], [1,0,0], [0,1,0], [0,0,1]])))
         R = np.zeros((6,12))
 
         R[[0,3,4],0] = PhiGrad.T[:, 0]
@@ -383,12 +391,7 @@ def generate_stiffness_matrix_paper(mesh):
         R[[4, 5, 2], 8] = PhiGrad.T[:, 2]
         R[[4, 5, 2], 11] = PhiGrad.T[:, 3]
 
-        C = D
-
-        stima3 = np.linalg.det(detgiv)/6*R.T.dot(C).dot(R)
-        local_K = stima3
-
-
+        local_K = np.linalg.det(detgiv)/6*R.T.dot(D).dot(R)
 
         for i in range(4):
             for j in range(4):
