298a4bdb142201c540ca898ce9cefd5034fc5dec
Leon Sievers <leon.sievers@tum.de>
2018-11-14 23:58:55 +0100

    WIP

--- project_2/main.py
+++ project_2/main.py
@@ -10,7 +10,7 @@ import numpy as np
 from project_2.infrastructure.mesh import Mesh
 from project_2.solvers.solver_quadpy import solve_quadpy
 from project_2.visualization.scatter_structure import plot_scatter_structure,plot_stress,plot_stress_meshed,export_matlab,trimeshit,trisurfit
-
+from project_2.infrastructure.configuration import Configuration
 
 def main():
     """
@@ -20,6 +20,7 @@ def main():
     parser.add_argument('-m', "--mesh", help="Load mesh", action='store_true')
     parser.add_argument('-sqp', "--solvequadpy", help="Uses quadpy to solve the problem", action='store_true')
     args = parser.parse_args()
+    basepath = "/home/leon/Documents/RCI/TMA4220_NumPDE/models/meshes/"
 
     if args.mesh:
         mesh = Mesh()
@@ -27,10 +28,11 @@ def main():
         mesh.plotMesh()
     if args.solvequadpy:
         mesh = Mesh()
-        mesh.loadMesh("/home/leon/Documents/RCI/TMA4220_NumPDE/models/export/giraffe.med")
-        #mesh.loadexamplemesh()
+        configuration = Configuration()
+        configuration.loadconfig(basepath+"beam_1.ini")
+        mesh.loadMesh(basepath+"beam_1.med")
         mesh.plotMesh()
-        stress, ux,uy,uz = solve_quadpy(mesh)
+        stress, ux,uy,uz = solve_quadpy(mesh,configuration)
         #plot_scatter_structure(mesh,ux,uy,uz)
         #plot_stress(mesh,stress)
         #plot_stress_meshed(mesh,stress)
--- project_2/solvers/solver_quadpy.py
+++ project_2/solvers/solver_quadpy.py
@@ -15,16 +15,27 @@ from project_2.infrastructure.affine_transformation import AffineTransformation
 
 
 
-def solve_quadpy(mesh,showstats=False):
+def solve_quadpy(mesh,config,showstats=False):
     """
     Solves the system using the quadpy pack
     :param mesh: The mesh on which to work
+    :param config: The configuration file
     :param showstats: Should stats be displayed
     :return: The solution
     """
+    Youngs_E_Modulus = config.emodulus
+    v = config.poissonratio
+    lame_lambda = Youngs_E_Modulus * v / ((1 + v) * (1 - 2 * v))
+    mue = Youngs_E_Modulus / (2 * (1 + v))
+    D = np.array([[lame_lambda + 2 * mue, lame_lambda, lame_lambda, 0, 0, 0],
+                  [lame_lambda, lame_lambda + 2 * mue, lame_lambda, 0, 0, 0],
+                  [lame_lambda, lame_lambda, lame_lambda + 2 * mue, 0, 0, 0], [0, 0, 0, mue, 0, 0],
+                  [0, 0, 0, 0, mue, 0], [0, 0, 0, 0, 0, mue]])
+
+
     print("[Info] Solving system using quadpy")
     print("[Info] Generating stiffness matrix")
-    K = generate_stiffness_matrix_paper(mesh)
+    K = generate_stiffness_matrix_paper(mesh,D)
 
 
     if showstats:
@@ -33,14 +44,28 @@ def solve_quadpy(mesh,showstats=False):
         print("[Info] Rank:"+str(np.linalg.matrix_rank(K))+"/"+str(K.shape[0]))
 
     print("[Info] Generating linear form")
-    b = generate_linear_form(mesh)
+    b = generate_linear_form(mesh,density=config.density)
 
     # BC Dirichlet
     nr = np.shape(mesh.supports)[0]
     bc_count = 0
+    nm_count = 0
+
+
+    A = 0.25*0.25
+    F = 10000
     for i in range(nr):
-        if True:
-            if mesh.supports[i, 2] < -0.25:
+        if False:
+            if mesh.supports[i, 0] > 0.25 and mesh.supports[i, 0] < 0.75 and mesh.supports[i, 1] > 0.25 and mesh.supports[i, 1] < 0.75 and mesh.supports[i, 2] == 1:
+                b[i*3+2] += F/A
+                nm_count+=1
+        if False:
+            if  mesh.supports[i, 2] == 0.01 and mesh.supports[i, 0] >=0.99:
+                b[i*3+2] += 100
+                nm_count+=1
+
+        if False:
+            if mesh.supports[i, 2] == 0:
                 bc_count+=1
                 K[i*3, :] = np.zeros((1, nr*3))
                 K[i*3, i*3] = 1
@@ -75,7 +100,7 @@ def solve_quadpy(mesh,showstats=False):
                 b[i*3] = 0
                 b[i*3+1] = 0
                 b[i*3+2] = 0
-        if False:
+        if True:
             if mesh.supports[i, 0] == 0 or mesh.supports[i, 0] == 0:
                 bc_count+=1
                 K[i*3, :] = np.zeros((1, nr*3))
@@ -88,7 +113,9 @@ def solve_quadpy(mesh,showstats=False):
                 b[i*3+1] = 0
                 b[i*3+2] = 0
 
+
     print("[Info] Imposed Dirichlet BC on "+str(bc_count)+" points")
+    print("[Info] Imposed Neumann BC on " + str(nm_count) + " points")
     if showstats:
         print("[Info] Data after applying BC")
         print("[Info] Condition number:"+str(np.linalg.cond(K)))
@@ -103,13 +130,13 @@ def solve_quadpy(mesh,showstats=False):
     uz = u[2::3]
 
     print("[Info] Calculating stress")
-    stress = generate_stress(ux,uy,uz,mesh)
+    stress = generate_stress(ux,uy,uz,mesh,D)
 
 
     return stress, ux, uy, uz
 
 
-def generate_stress(ux,uy,uz,mesh):
+def generate_stress(ux,uy,uz,mesh,D):
     varnr = mesh.tetraeders.shape[0]
     p1_ref = P1ReferenceElement()
     atraf = AffineTransformation()
@@ -117,14 +144,7 @@ def generate_stress(ux,uy,uz,mesh):
     strain = np.zeros((varnr,6))
     stress = np.zeros((varnr, 6))
 
-    Youngs_E_Modulus = 250 * 10 ** 9
-    v = 0.3
-    lame_lambda = Youngs_E_Modulus * v / ((1 + v) * (1 - 2 * v))
-    mue = Youngs_E_Modulus / (2 * (1 + v))
-    D = np.array([[lame_lambda + 2 * mue, lame_lambda, lame_lambda, 0, 0, 0],
-                  [lame_lambda, lame_lambda + 2 * mue, lame_lambda, 0, 0, 0],
-                  [lame_lambda, lame_lambda, lame_lambda + 2 * mue, 0, 0, 0], [0, 0, 0, mue, 0, 0],
-                  [0, 0, 0, 0, mue, 0], [0, 0, 0, 0, 0, mue]])
+
 
     co = (.1, .1, .1)
 
@@ -175,13 +195,13 @@ def generate_stress(ux,uy,uz,mesh):
     return stress
 
 
-def generate_linear_form(mesh):
+def generate_linear_form(mesh,density):
     def f1(x=0):
         return 0
     def f2(x=0):
         return 0
-    def f3(x=0):
-        return -9.81
+    def f3(density,x=0):
+        return -9.81*density
 
     atraf = AffineTransformation()
     varnr = mesh.supports.shape[0]
@@ -197,167 +217,14 @@ def generate_linear_form(mesh):
         for i in range(4):
             b[mesh.tetraeders[n,i]*3] += f1()*0.5*det
             b[mesh.tetraeders[n,i]*3 + 1] += f1() * 0.5 * det
-            b[mesh.tetraeders[n,i]*3 + 2] += f3() * 0.5 * det
+            b[mesh.tetraeders[n,i]*3 + 2] += f3(density) * 0.5 * det
     return b
 
 
-def generate_stiffness_matrix(mesh):
+
+def generate_stiffness_matrix_paper(mesh,D):
     varnr = mesh.supports.shape[0]
-    p1_ref = P1ReferenceElement()
-    #K = np.zeros((varnr*3, varnr*3))
-    #K = lil_matrix(varnr*3,varnr*3)
-    K = csr_matrix(varnr*3,varnr*3)
-    atraf = AffineTransformation()
-    for n in range(mesh.tetraeders.shape[0]):
-        v0_coord = (mesh.supports[mesh.tetraeders[n,0],0],mesh.supports[mesh.tetraeders[n,0],1],mesh.supports[mesh.tetraeders[n,0],2])
-        v1_coord = (mesh.supports[mesh.tetraeders[n,1],0],mesh.supports[mesh.tetraeders[n,1],1],mesh.supports[mesh.tetraeders[n,1],2])
-        v2_coord = (mesh.supports[mesh.tetraeders[n,2],0],mesh.supports[mesh.tetraeders[n,2],1],mesh.supports[mesh.tetraeders[n,2],2])
-        v3_coord = (mesh.supports[mesh.tetraeders[n,3],0],mesh.supports[mesh.tetraeders[n,3],1],mesh.supports[mesh.tetraeders[n,3],2])
-
-        atraf.set_target_cell(v0_coord, v1_coord, v2_coord,v3_coord)
-        jinvt = atraf.get_inverse_jacobian().T
-
-
-        Youngs_E_Modulus = 250 * 10**9
-        v = 0.3
-
-        lame_lambda = Youngs_E_Modulus * v / ((1 + v) * (1 - 2 * v))
-        mue = Youngs_E_Modulus/(2*(1+v))
-
-        D_0 = np.array([[1, v, v, 0, 0, 0], [v, 1, v, 0, 0, 0], [v, v, 1, 0, 0, 0],[0,0,0,(1 - v) / 2,0,0],[0,0,0,0,(1 - v) / 2,0],[0,0,0,0,0,(1 - v) / 2]])
-        D = Youngs_E_Modulus / (1 - v ** 2) * D_0
-        D_00 = np.array([[lame_lambda+2*mue, lame_lambda, lame_lambda, 0, 0, 0], [lame_lambda, lame_lambda+2*mue, lame_lambda, 0, 0, 0], [lame_lambda, lame_lambda, lame_lambda+2*mue, 0, 0, 0],[0,0,0,mue,0,0],[0,0,0,0,mue,0],[0,0,0,0,0,mue]])
-        #D = Youngs_E_Modulus / (1 - v ** 2) * D_0
-        D = D_00
-
-        lame_lambda = Youngs_E_Modulus * v / ((1 + v) * (1 - 2 * v))
-        mue = Youngs_E_Modulus/(2*(1+v))
-
-        D_00 = np.array([[lame_lambda+2*mue, lame_lambda, lame_lambda, 0, 0, 0], [lame_lambda, lame_lambda+2*mue, lame_lambda, 0, 0, 0], [lame_lambda, lame_lambda, lame_lambda+2*mue, 0, 0, 0],[0,0,0,mue,0,0],[0,0,0,0,mue,0],[0,0,0,0,0,mue]])
-        D = D_00
-
-        PhiGrad = np.ones((4,4))
-        vertices_ = np.squeeze(np.array([v0_coord, v1_coord, v2_coord,v3_coord]))
-        PhiGrad[1:4,0:] = vertices_.T
-        detgiv = PhiGrad
-        PhiGrad = np.linalg.inv(PhiGrad).dot(np.squeeze(np.array([[0,0,0], [1,0,0], [0,1,0], [0,0,1]])))
-
-
-        R = np.zeros((6,12))
-
-        R[[0,3,4],0] = PhiGrad.T[:, 0]
-        R[[0, 3, 4], 3] = PhiGrad.T[:, 1]
-        R[[0, 3, 4], 6] = PhiGrad.T[:, 2]
-        R[[0, 3, 4], 9] = PhiGrad.T[:, 3]
-
-        R[[3,1,5],1] = PhiGrad.T[:, 0]
-        R[[3, 1, 5], 4] = PhiGrad.T[:, 1]
-        R[[3, 1, 5], 7] = PhiGrad.T[:, 2]
-        R[[3, 1, 5], 10] = PhiGrad.T[:, 3]
-
-        R[[4,5,2],2] = PhiGrad.T[:, 0]
-        R[[4, 5, 2], 5] = PhiGrad.T[:, 1]
-        R[[4, 5, 2], 8] = PhiGrad.T[:, 2]
-        R[[4, 5, 2], 11] = PhiGrad.T[:, 3]
-
-
-        local_K = np.zeros((12,12))
-        for i in range(12):
-            for j in range(12):
-                co = (0.1, 0.1, 0.1)
-
-                B_i= np.zeros((6, 1))
-                B_j = np.zeros((6, 1))
-
-                grad = np.matrix('-1 1 0 0; -1 0 1 0; -1 0 0 1');
-
-
-                if i % 3 == 0:
-                    B_i[0, :] = jinvt.dot(p1_ref.gradients(co)[:, i // 3])[0]
-                    B_i[3, :] = jinvt.dot(p1_ref.gradients(co)[:, i // 3])[1]
-                    B_i[4, :] = jinvt.dot(p1_ref.gradients(co)[:, i // 3])[2]
-                elif i%3 == 1:
-                    B_i[1, :] = jinvt.dot(p1_ref.gradients(co)[:, i // 3])[1]
-                    B_i[3, :] = jinvt.dot(p1_ref.gradients(co)[:, i // 3])[0]
-                    B_i[5, :] = jinvt.dot(p1_ref.gradients(co)[:, i // 3])[2]
-                else:
-                    B_i[2, :] = jinvt.dot(p1_ref.gradients(co)[:, i // 3])[2]
-                    B_i[4, :] = jinvt.dot(p1_ref.gradients(co)[:, i // 3])[0]
-                    B_i[5, :] = jinvt.dot(p1_ref.gradients(co)[:, i // 3])[1]
-
-                if j % 3 == 0:
-                    B_j[0, :] = jinvt.dot(p1_ref.gradients(co)[:, j // 3])[0]
-                    B_j[3, :] = jinvt.dot(p1_ref.gradients(co)[:, j // 3])[1]
-                    B_j[4, :] = jinvt.dot(p1_ref.gradients(co)[:, j // 3])[2]
-                elif j % 3 == 1:
-                    B_j[1, :] = jinvt.dot(p1_ref.gradients(co)[:, j // 3])[1]
-                    B_j[3, :] = jinvt.dot(p1_ref.gradients(co)[:, j // 3])[0]
-                    B_j[5, :] = jinvt.dot(p1_ref.gradients(co)[:, j // 3])[2]
-                else:
-                    B_j[2, :] = jinvt.dot(p1_ref.gradients(co)[:, j // 3])[2]
-                    B_j[4, :] = jinvt.dot(p1_ref.gradients(co)[:, j // 3])[0]
-                    B_j[5, :] = jinvt.dot(p1_ref.gradients(co)[:, j // 3])[1]
-
-
-
-
-
-                result = np.asscalar(B_i.T.dot(D).dot(B_j))
-                ans = result/2
-
-                local_K[i,j] =atraf.get_determinant() * ans/3
-
-
-
-
-
-
-
-
-        C = D
-
-
-        stima3 = np.linalg.det(detgiv)/6*R.T.dot(C).dot(R)
-        print(stima3-local_K)
-        print()
-
-        exit(0)
-
-
-        for i in range(4):
-            for j in range(4):
-                K[mesh.tetraeders[n,i]*3, mesh.tetraeders[n,j]*3] += local_K[i*3,j*3]
-                K[mesh.tetraeders[n,i] * 3 + 1, mesh.tetraeders[n,j] * 3 + 1] += local_K[i * 3+1, j * 3+1]
-                K[mesh.tetraeders[n, i] * 3 + 2, mesh.tetraeders[n, j] * 3 + 2] += local_K[i * 3 + 2, j * 3 + 2]
-
-
-
-    return K
-
-def generate_stiffness_matrix_paper(mesh):
-    varnr = mesh.supports.shape[0]
-    p1_ref = P1ReferenceElement()
-    #K = np.zeros((varnr*3, varnr*3))
     K = lil_matrix((varnr * 3, varnr * 3))
-    #K = csr_matrix((varnr*3,varnr*3))
-    atraf = AffineTransformation()
-
-    Youngs_E_Modulus = 250 * 10 ** 9
-    v = 0.3
-
-    lame_lambda = Youngs_E_Modulus * v / ((1 + v) * (1 - 2 * v))
-    mue = Youngs_E_Modulus / (2 * (1 + v))
-
-    fracter = 1
-    D_0 = np.array([[1, v, v, 0, 0, 0], [v, 1, v, 0, 0, 0], [v, v, 1, 0, 0, 0], [0, 0, 0, (1 - v) / fracter, 0, 0],
-                    [0, 0, 0, 0, (1 - fracter) / 2, 0], [0, 0, 0, 0, 0, (1 - v) / fracter]])
-    D_00 = np.array([[lame_lambda + 2 * mue, lame_lambda, lame_lambda, 0, 0, 0],
-                     [lame_lambda, lame_lambda + 2 * mue, lame_lambda, 0, 0, 0],
-                     [lame_lambda, lame_lambda, lame_lambda + 2 * mue, 0, 0, 0], [0, 0, 0, mue, 0, 0],
-                     [0, 0, 0, 0, mue, 0], [0, 0, 0, 0, 0, mue]])
-    # D = Youngs_E_Modulus / (1 - v ** 2) * D_0
-    D = D_00
-
 
 
     for n in range(mesh.tetraeders.shape[0]):
